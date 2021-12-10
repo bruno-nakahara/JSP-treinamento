7420c1d5b1a2dc81fafc5c1b4f9e813e65ca887c
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-12-09 23:38:44 -0300

    Mantendo os dados em tela e adicionando campos na tabela do usuário

--- curso-jsp/src/main/java/servlets/ServletUsuarioController.java
+++ curso-jsp/src/main/java/servlets/ServletUsuarioController.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
+@WebServlet("/ServletUsuarioController")
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -31,14 +33,16 @@ public class ServletUsuarioController extends HttpServlet {
 		String senha = request.getParameter("senha");
 		
 		ModelLogin modelLogin = new ModelLogin();
+		
 		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
 		modelLogin.setNome(nome);
 		modelLogin.setEmail(email);
 		modelLogin.setLogin(login);
 		modelLogin.setSenha(senha);
 		
-		RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
-		redireciona.forward(request, response);
+		request.setAttribute("modelLogin", modelLogin);
+		
+		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
 		
 	}
 
